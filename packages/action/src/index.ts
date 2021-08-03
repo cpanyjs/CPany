b1b67f5653d0ae28098efc0e7148c83696308368
XLor <yjl9903@vip.qq.com>
2021-08-04 01:29:50 +0800

    add: load config

--- packages/action/src/index.ts
+++ packages/action/src/index.ts
@@ -1,9 +1,21 @@
 import * as core from '@actions/core';
+import { readFileSync } from 'fs';
+import { load } from 'js-yaml';
+
 import { hello } from '@cpany/core';
+
+async function getConfig(path: string) {
+  const content = readFileSync(path, 'utf8');
+  return load(content);
+}
 
 async function run() {
   hello();
   core.info('This is an action');
+
+  const configPath = core.getInput('config');
+  const config = await getConfig(configPath);
+  core.info(JSON.stringify(config, null, 2));
 }
 
 run();
