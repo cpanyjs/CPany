b1392338e978e9ebf979aec3138206027b2dfbbc
XLor <yjl9903@vip.qq.com>
2021-08-15 00:54:51 +0800

    add: table component

--- packages/app/src/pages/Members.vue
+++ packages/app/src/pages/Members.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2 class="mb-4">成员</h2>
-    <div
+    <!-- <div
       v-for="user in users"
       :key="user.name"
       class="p-2 border border-gray-300 rounded mb-2"
@@ -13,12 +13,29 @@
           handle.handle
         }}</span>
       </p>
-    </div>
+    </div> -->
+    <c-table :data="users">
+      <template #columns="{ index, row }">
+        <c-table-column label="#" width="4em" align="center"
+          ><span class="font-600">{{ index + 1 }}</span></c-table-column
+        >
+        <c-table-column label="姓名">
+          {{ row.name }}
+        </c-table-column>
+        <c-table-column label="提交总数" width="6em" align="center">{{
+          row.submissions.length
+        }}</c-table-column>
+        <c-table-column label="比赛场次" width="6em" align="center">{{
+          row.contests.length
+        }}</c-table-column>
+      </template>
+    </c-table>
   </div>
 </template>
 
 <script setup lang="ts">
 import { users } from '../overview';
+import { CTable, CTableColumn } from '../components/table';
 </script>
 
 <style></style>
