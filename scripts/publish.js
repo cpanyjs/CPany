c501b1af6b5eb29aeebd1b0bcf1bc7264425675f
XLor <yjl9903@vip.qq.com>
2021-08-16 16:08:00 +0800

    release: v0.0.7

---
+++ scripts/publish.js
@@ -0,0 +1,32 @@
+const { join } = require('path');
+const { readJSON, writeJSON } = require('fs-extra');
+const execa = require('execa');
+
+const packages = [
+  './packages/action',
+  './packages/app',
+  './packages/cli',
+  './packages/codeforces',
+  './packages/core',
+  './packages/types'
+];
+
+async function run() {
+  const version = process.argv[2];
+  if (!version || !/^\d+.\d+.\d+/.test(version)) {
+    console.error(`Invalid version: ${version}`);
+    process.exit(1);
+  }
+  console.log('Publish @cpany version:', version);
+  for (const package of packages) {
+    const path = join(package, 'package.json');
+    const json = await readJSON(path);
+    json.version = version;
+    await writeJSON(path, json, { spaces: 2 });
+  }
+  await execa('git', ['add', '.']);
+  await execa('git', ['commit', '-m', `release: v${version}`]);
+  await execa('git', ['tag', '-a', `v${version}`, '-m', `release: v${version}`]);
+}
+
+run();
