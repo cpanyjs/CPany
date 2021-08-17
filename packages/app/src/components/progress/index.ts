1be08abfb604495da8ebee8bd00ce5eedb244145
XLor <yjl9903@vip.qq.com>
2021-08-17 22:27:44 +0800

    update: add load cf standing

--- packages/app/src/components/progress/index.ts
+++ packages/app/src/components/progress/index.ts
@@ -1,3 +1,17 @@
 import Progress from './progress.vue';
+import { inject } from 'vue';
+import type { Ref } from 'vue';
 
 export { Progress };
+
+export function useGlobalLoading() {
+  const loading = inject<Ref<boolean>>('loading');
+  return {
+    start() {
+      loading!.value = true;
+    },
+    end() {
+      loading!.value = false;
+    }
+  };
+}
