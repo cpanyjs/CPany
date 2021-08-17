a91bcc59d24f04b435ef7ed352aa1918ff472899
XLor <yjl9903@vip.qq.com>
2021-08-17 18:12:24 +0800

    fix: team name spacing

--- packages/app/src/pages/Contest/TeamName.vue
+++ packages/app/src/pages/Contest/TeamName.vue
@@ -3,7 +3,8 @@
     <div>{{ author.teamName }}</div>
     <div class="space-left">
       <user-link
-        v-for="user in author.members"
+        v-for="(user, index) in author.members"
+        :class="index > 0 && 'ml-2'"
         :key="user"
         :name="user"
       ></user-link>
@@ -19,12 +20,3 @@ import UserLink from '@/components/user-link.vue';
 
 defineProps<{ author: IAuthor }>();
 </script>
-
-<style>
-.space-left > a {
-  @apply ml-2;
-}
-.space-left > a:first-child {
-  @apply ml-0;
-}
-</style>
