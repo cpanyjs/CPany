e9a1f9d10f998ee66d218ccdc1080e6f75032bcf
XLor <yjl9903@vip.qq.com>
2021-08-17 21:37:50 +0800

    add: fetch cf standings

---
+++ packages/app/src/cfHandles.ts
@@ -0,0 +1,4 @@
+import type { CodeforcesHandleList } from '@cpany/types';
+import rawHandles from './cpany/cfHandles.json';
+
+export const handles = rawHandles as CodeforcesHandleList;
--- packages/app/src/pages/Contest/Codeforces.vue
+++ packages/app/src/pages/Contest/Codeforces.vue
@@ -7,17 +7,92 @@
 </template>
 
 <script setup lang="ts">
-import type { IContest, RouteKey } from '@cpany/types';
+import type { IContest, IContestProblem, RouteKey } from '@cpany/types';
+import { Verdict } from '@cpany/types';
+
 import { useRoute } from 'vue-router';
 import { ref, watch } from 'vue';
 import axios from 'axios';
 
 import Page from './Contest.vue';
-import { findCodeforces } from '../../codeforces';
+import { findCodeforces } from '@/codeforces';
+import { handles } from '@/cfHandles';
+import { CodeforcesAPIBase } from '@/config';
 
 const route = useRoute();
 
 const contest = ref<RouteKey<IContest> | null>(null);
+
+const fetchStanding = async (contest: RouteKey<IContest>) => {
+  const api = axios.create({ baseURL: CodeforcesAPIBase });
+
+  const {
+    data: { result }
+  } = await api.get('contest.standings', {
+    params: {
+      contestId: contest.id,
+      handles: handles.map(({ h }) => h).join(';')
+    }
+  });
+
+  contest.problems = result.problems as IContestProblem[];
+  for (const prob of result.problems) {
+    prob.problemUrl = `https://codeforces.com/contest/${prob.contestId}/problem/${prob.index}`;
+  }
+  contest.standings = [];
+  for (const row of result.rows) {
+    const penalty = row.problemResults.reduce((sum: number, result: any) => {
+      if (result.points === 0) return sum;
+      return (
+        sum +
+        (result.bestSubmissionTimeSeconds ?? 0) +
+        20 * (result.rejectedAttemptCount ?? 0)
+      );
+    }, 0);
+
+    contest.standings!.push({
+      author: {
+        members: row.party.members.map(
+          (handle: { handle: string }) => handle.handle
+        ),
+        teamName: row.party.teamName,
+        participantTime: row.party.participantTime ?? contest.startTime,
+        participantType: row.party.participantType
+      },
+      rank: row.rank,
+      solved: row.problemResults.filter(
+        (result: { points: number }) => result.points > 0
+      ).length,
+      penalty,
+      submissions: row.problemResults
+        .map((result: any, index: number) => {
+          if (result.points > 0) {
+            return {
+              id: -1,
+              creationTime:
+                result.bestSubmissionTimeSeconds + contest.startTime,
+              relativeTime: result.bestSubmissionTimeSeconds,
+              problemIndex: index,
+              verdict: Verdict.OK,
+              dirty: result.rejectedAttemptCount
+            };
+          } else if (result.rejectedAttemptCount > 0) {
+            return {
+              id: -1,
+              creationTime:
+                result.bestSubmissionTimeSeconds + contest.startTime,
+              relativeTime: result.bestSubmissionTimeSeconds,
+              problemIndex: index,
+              dirty: result.rejectedAttemptCount
+            };
+          } else {
+            return null;
+          }
+        })
+        .filter((result: any) => result !== null)
+    });
+  }
+};
 
 watch(
   () => route.params,
@@ -26,6 +101,7 @@ watch(
       const cf = findCodeforces(+newParams.id);
       if (cf !== null) {
         contest.value = cf;
+        fetchStanding(contest.value);
         // Dep: manual update document title
         document.title = `${cf.name} - CPany`;
       }
