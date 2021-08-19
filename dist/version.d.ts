5d61e8011edc2b2fc4cd9608d97d0d5fee38aa74
XLor <yjl9903@vip.qq.com>
2021-08-19 16:44:19 +0800

    release: v0.0.25

--- dist/version.d.ts
+++ dist/version.d.ts
@@ -1,1 +1,1 @@
-export declare const ActionVersion = "0.0.24";
+export declare const ActionVersion = "0.0.25";
--- packages/action/src/version.ts
+++ packages/action/src/version.ts
@@ -1,1 +1,1 @@
-export const ActionVersion = '0.0.24';
+export const ActionVersion = '0.0.25';
