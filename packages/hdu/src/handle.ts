a1296f772ad473f255665064aa6e42c2bf9067b1
XLor <yjl9903@vip.qq.com>
2021-08-26 18:55:37 +0800

    update: add fetch submission

--- packages/hdu/src/handle.ts
+++ packages/hdu/src/handle.ts
@@ -1,6 +1,8 @@
 import type { IPlugin } from '@cpany/core';
 import type { IHandleWithHdu } from '@cpany/types/hdu';
+import { ISubmission, ParticipantType, Verdict } from '@cpany/types';
 import axios from 'axios';
+import { parse } from 'node-html-parser';
 
 export function createHduHandlePlugin(): IPlugin {
   const name = 'hdu/handle';
@@ -15,6 +17,7 @@ export function createHduHandlePlugin(): IPlugin {
     async transform({ id, type }) {
       if (type === name) {
         const handle = await fetchHandle(id);
+        handle.submissions = await fetchSubmissions(id);
         return {
           key: gid(id),
           content: JSON.stringify(handle, null, 2)
@@ -41,4 +44,80 @@ export async function fetchHandle(handle: string): Promise<IHandleWithHdu> {
     handleUrl: `https://acm.hdu.edu.cn/userstatus.php?user=${handle}`,
     submissions: []
   };
+}
+
+export async function fetchSubmissions(handle: string): Promise<ISubmission[]> {
+  const subs: ISubmission[] = [];
+  const fetch = async (first?: number) => {
+    let minId = first ?? Number.MAX_VALUE;
+
+    const { data } = await axios.get(`https://acm.hdu.edu.cn/status.php`, {
+      params: { user: handle, first }
+    });
+
+    const root = parse(data);
+    const items = root
+      .querySelectorAll('tr[align]')
+      .filter((node) => node.childNodes.length === 9);
+    for (const node of items) {
+      const id = +node.childNodes[0].innerText;
+      const language = node.childNodes[7].innerText;
+      const time = new Date(node.childNodes[1].innerText).getTime() / 1000;
+      const pid = +node.childNodes[3].childNodes[0].innerText;
+
+      const verdict = node.childNodes[2].childNodes[0].innerText;
+      // skip waiting
+      if (
+        verdict === 'Queuing' ||
+        verdict === 'Compiling' ||
+        verdict === 'Running'
+      )
+        continue;
+
+      subs.push({
+        type: 'hdu',
+        id,
+        creationTime: time,
+        language,
+        verdict: parseVerdict(verdict),
+        author: {
+          members: [handle],
+          participantType: ParticipantType.PRACTICE,
+          participantTime: time
+        },
+        problem: {
+          type: 'hdu',
+          id: pid,
+          name: `${pid}`,
+          problemUrl: `https://acm.hdu.edu.cn/showproblem.php?pid=${pid}`
+        },
+        submissionUrl: `https://acm.hdu.edu.cn/viewcode.php?rid=${id}`
+      });
+
+      minId = Math.min(minId, id);
+    }
+
+    return minId - 1;
+  };
+
+  let curId: number | undefined = undefined;
+  while (true) {
+    const oldLen = subs.length;
+    curId = await fetch(curId);
+    if (subs.length === oldLen) break;
+  }
+
+  return subs;
+}
+
+function parseVerdict(verdict: string) {
+  if (verdict === 'Accepted') return Verdict.OK;
+  if (verdict === 'Presentation Error') return Verdict.WRONG_ANSWER;
+  if (verdict === 'Wrong Answer') return Verdict.WRONG_ANSWER;
+  if (verdict === 'Runtime Error') return Verdict.RUNTIME_ERROR;
+  if (verdict === 'Time Limit Exceeded') return Verdict.TIME_LIMIT_EXCEEDED;
+  if (verdict === 'Memory Limit Exceeded') return Verdict.MEMORY_LIMIT_EXCEEDED;
+  if (verdict === 'Output Limit Exceeded') return Verdict.FAILED;
+  if (verdict === 'Compilation Error') return Verdict.COMPILATION_ERROR;
+  return Verdict.FAILED;
 }
