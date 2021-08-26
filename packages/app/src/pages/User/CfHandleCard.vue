66aa121082ae3f951f496597563c960fe2b14a22
XLor <yjl9903@vip.qq.com>
2021-08-26 21:26:49 +0800

    update: add count in handle card

--- packages/app/src/pages/User/CfHandleCard.vue
+++ packages/app/src/pages/User/CfHandleCard.vue
@@ -13,10 +13,22 @@
       handle.codeforces.maxRating
     }}</cf-rating-color>
   </p>
+  <p>
+    <span class="font-600">提交: </span>
+    <span>{{ handle.submissions.length }}</span>
+  </p>
+  <p>
+    <span class="font-600">通过: </span>
+    <span>{{
+      handle.submissions.filter(({ verdict }) => verdict === Verdict.OK).length
+    }}</span>
+  </p>
 </template>
 
 <script setup lang="ts">
 import type { IHandleWithCodeforces } from '@cpany/types/codeforces';
+import { Verdict } from '@cpany/types';
+
 import { CfHandle, CfRatingColor } from '@/components/codeforces';
 
 defineProps<{ handle: IHandleWithCodeforces }>();
--- packages/app/src/pages/User/HandleCard.vue
+++ packages/app/src/pages/User/HandleCard.vue
@@ -11,6 +11,16 @@
       <span class="font-600">Rank: </span>
       <span>{{ hdu.hdu.rank }}</span>
     </p>
+    <p>
+      <span class="font-600">提交: </span>
+      <span>{{ hdu.submissions.length }}</span>
+    </p>
+    <p>
+      <span class="font-600">通过: </span>
+      <span>{{
+        hdu.submissions.filter(({ verdict }) => verdict === Verdict.OK).length
+      }}</span>
+    </p>
   </template>
   <template v-else>
     <p>
@@ -24,6 +34,8 @@
 import type { IHandle } from '@cpany/types';
 import type { IHandleWithCodeforces } from '@cpany/types/codeforces';
 import type { IHandleWithHdu } from '@cpany/types/hdu';
+import { Verdict } from '@cpany/types';
+
 import { toRefs, computed } from 'vue';
 import CfHandleCard from './CfHandleCard.vue';
 
