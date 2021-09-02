def15e2221906766ef56b8732f9e5f4bd952c4de
XLor <yjl9903@vip.qq.com>
2021-09-02 23:18:36 +0800

    fix: release.js

--- scripts/release.js
+++ scripts/release.js
@@ -7,6 +7,9 @@ const packages = [
   './packages/app',
   './packages/cli',
   './packages/codeforces',
+  './packages/hdu',
+  './packages/luogu',
+  './packages/utils',
   './packages/core',
   './packages/types'
 ];
