89a8ef9000e58aef7587634477f1b8724a55ac9e
XLor <yjl9903@vip.qq.com>
2021-09-30 17:28:59 +0800

    chore: update release script

--- scripts/release.js
+++ scripts/release.js
@@ -13,7 +13,7 @@ const packages = [
   './packages/utils',
   './packages/core',
   './packages/types',
-  './packages/compress',
+  './packages/compress'
 ];
 
 async function run() {
@@ -23,16 +23,16 @@ async function run() {
     console.error(`Invalid version: ${version}`);
     process.exit(1);
   }
-  
+
   console.log('Publish @cpany version:', version);
-  
+
   for (const package of packages) {
     const path = join(package, 'package.json');
     const json = await readJSON(path);
     json.version = version;
     await writeJSON(path, json, { spaces: 2 });
   }
-  
+
   await writeFile('./packages/cli/.env', `VITE_CLI_VERSION=${version}`);
   await writeFile('./packages/action/src/version.ts', `export const ActionVersion = '${version}';`);
 
@@ -40,11 +40,15 @@ async function run() {
     .toString()
     .replace(/yjl9903\/CPany@v\d+\.\d+\.\d+/, `yjl9903/CPany@v${version}`);
   await writeFile('./README.md', readme);
-  await writeFile('./packages/cli/README.md', readme)
-  
+  await writeFile('./packages/cli/README.md', readme);
+
+  await execa('npm', ['run', 'build']);
+
   await execa('git', ['add', '.'], { stdio: 'inherit' });
   await execa('git', ['commit', '-m', `release: v${version}`], { stdio: 'inherit' });
-  await execa('git', ['tag', '-a', `v${version}`, '-m', `release: v${version}`], { stdio: 'inherit' });
+  await execa('git', ['tag', '-a', `v${version}`, '-m', `release: v${version}`], {
+    stdio: 'inherit'
+  });
 }
 
 run();
