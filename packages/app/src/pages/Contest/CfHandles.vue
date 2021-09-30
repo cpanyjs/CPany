ceff15fb379abb3159d904d84ec518cb49a4e75a
XLor <yjl9903@vip.qq.com>
2021-09-30 19:16:06 +0800

    fix(app): cfHandles auto next line

--- packages/app/src/pages/Contest/CfHandles.vue
+++ packages/app/src/pages/Contest/CfHandles.vue
@@ -1,5 +1,5 @@
 <template>
-  <template v-if="isDef(author.teamName)">
+  <template v-if="isDef(author.teamName) && author.members.length <= 1">
     <user-link :name="author.teamName"></user-link>
     <span class="space-left">
       <a
@@ -12,6 +12,22 @@
         <cf-rating-color :rating="findHandleRating(handle)">{{ handle }}</cf-rating-color>
       </a>
     </span>
+  </template>
+  <template v-else-if="isDef(author.teamName)">
+    <div>
+      <user-link :name="author.teamName"></user-link>
+    </div>
+    <div class="space-left">
+      <a
+        v-for="(handle, index) in author.members"
+        :key="index"
+        class="mr-2"
+        :href="`https://codeforces.com/profile/${handle}`"
+        target="_blank"
+      >
+        <cf-rating-color :rating="findHandleRating(handle)">{{ handle }}</cf-rating-color>
+      </a>
+    </div>
   </template>
   <span v-else>{{ author.members[0] }}</span>
 </template>
