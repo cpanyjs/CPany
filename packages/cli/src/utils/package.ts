118543a3c5d358583a4f34416e12d8a97b060371
XLor <yjl9903@vip.qq.com>
2021-11-21 01:33:03 +0800

    fix(cli): flattern dir

--- packages/cli/src/utils/package.ts
+++ packages/cli/src/utils/package.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { readFileSync } from 'fs';
 
 export const packageInfo = JSON.parse(
-  readFileSync(path.resolve(__dirname, '../../package.json'), 'utf-8')
+  readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8')
 );
 
 export const dependencies = packageInfo.dependencies;
