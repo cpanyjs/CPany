4944152b2ed6ff413f48f5cd15630453fe18d392
yjl9903 <yjl9903@users.noreply.github.com>
2022-01-14 19:58:38 +0800

    deploy: f7ea756588b9234c76dcf5233d3067a285eaa16c

--- assets/About.00ef2a4c.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./index.1ecc0a69.js";import{o as r,b as s,d as o}from"./vendor.1d8aaf39.js";const e={},n=[o("h2",{class:"mb-4"},"关于",-1)];var d=a(e,[["render",function(a,o){return r(),s("div",null,n)}]]);export{d as default};
