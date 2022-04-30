a0bf2da019c8592f2f063b1b253b6ca996fc3095
XLor <yjl9903@vip.qq.com>
2022-04-30 22:36:14 +0800

    feat(nowcoder): proxy

--- packages/nowcoder/src/constant.ts
+++ packages/nowcoder/src/constant.ts
@@ -3,9 +3,28 @@ import axiosRetry from 'axios-retry';
 
 export const nowcoder = 'nowcoder';
 
+function getProxy() {
+  const proxy = process.env.HTTPS_PROXY || process.env.HTTP_PROXY;
+  if (proxy) {
+    const match = /^https?:\/\/(\d+\.\d+\.\d+\.\d+):(\d+)$/.exec(proxy);
+    if (match) {
+      return {
+        protocol: match[1],
+        host: match[2],
+        port: +match[3]
+      };
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
 export const api = axios.create({
   baseURL: 'https://ac.nowcoder.com',
-  timeout: 10 * 1000
+  timeout: 10 * 1000,
+  proxy: getProxy()
 });
 
 axiosRetry(api, { retries: 10 });
