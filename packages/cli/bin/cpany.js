5e06b28dfad13ce8610e79845a6ade029987a8b4
XLor <yjl9903@vip.qq.com>
2022-05-09 14:59:13 +0800

    fix: cli bin

--- packages/cli/bin/cpany.js
+++ packages/cli/bin/cpany.js
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
-'use strict';
 
-const path = require('path');
-const resolveFrom = require('resolve-from');
-
-let modulePath = '../dist/cli';
-try {
-  // use local cli if exists
-  modulePath = path.join(path.dirname(resolveFrom(process.cwd(), '@cpany/cli')), 'cli.js');
-} catch {}
-
-require(modulePath);
+import('../dist/cli.js');
