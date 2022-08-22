09501f919650e16c827cdc9edbc57fd9cbfb7866
yjl9903 <yjl9903@users.noreply.github.com>
2022-08-22 11:12:13 +0800

    deploy: 371277eb689d88955949115e8673a5bebf151519

---
+++ assets/About.beab49a2.js
@@ -0,0 +1,1 @@
+import{_ as e,o,e as s,f as t}from"./index.5aab9a7e.js";const c={},a=t("h2",{class:"mb-4"},"\u5173\u4E8E",-1),r=[a];function _(n,d){return o(),s("div",null,r)}var l=e(c,[["render",_]]);export{l as default};
