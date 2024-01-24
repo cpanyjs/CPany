6dda05005d812e034fab9c933686bed490949de2
yjl9903 <yjl9903@users.noreply.github.com>
2024-01-24 13:37:13 +0800

    deploy: f67df5f09cdec58430ad67ea603fe5540eeb183f

---
+++ assets/account.37184596.js
@@ -0,0 +1,1 @@
+import{o as e,c as o,e as c}from"./index.6fe21063.js";const t={viewBox:"0 0 24 24",width:"1.2em",height:"1.2em"},a=c("path",{fill:"currentColor",d:"M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4"},null,-1),n=[a];function r(s,i){return e(),o("svg",t,[...n])}var h={name:"mdi-account",render:r};export{h as I};
